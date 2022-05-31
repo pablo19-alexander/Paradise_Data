e01b2952f46b4324c5aec180ae0f38c8e31ceb53
pablo salazar <pablo.pass1905@gmail.com>
2022-05-31 10:44:15 -0500

    correciones

--- php/index.php
+++ php/index.php
@@ -223,8 +223,11 @@
         </section>
         <!-- Footer-->
         <footer class="footer bg-black small text-center text-white-50">
-            <div class="container px-4 px-lg-5">Copyright &copy; ParadiseData 2022</div>
-        </footer>
+        <div class="container px-4 px-lg-5">Copyright &copy; paradiseData
+            <script>
+                document.write(new Date().getFullYear())
+            </script>
+        </div>        </footer>
         <!-- Bootstrap core JS-->
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
         <!-- Core theme JS-->
